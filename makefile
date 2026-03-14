93e81a142313e582e16dd204a194bc907adce944
Adnan <adnanadel439@gmail.com>
2026-03-15 00:52:20 +0200

    updates in main

--- makefile
+++ makefile
@@ -1,14 +1,15 @@
-CPP = g++
-CPPFLAGS = -Wall -Wextra -std=c++17 `sdl2-config --cflags`
-LDFLAGS = `sdl2-config --libs`
-SRC_DIR = src
-BUILD_DIR = build
+CPP      = g++
+CPPFLAGS = -Wall -Wextra -Wpedantic -std=c++17 $(shell sdl2-config --cflags)
+LDFLAGS  = $(shell sdl2-config --libs)
+
+SRC_DIR     = src
+BUILD_DIR   = build
 INCLUDE_DIR = include
 
 SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
 OBJ_FILES = $(patsubst $(SRC_DIR)/%.cpp, $(BUILD_DIR)/%.o, $(SRC_FILES))
 
-TARGET = chip8-emulator
+TARGET       = chip8-emulator
 DEBUG_TARGET = chip8-emulator-debug
 
 all: $(TARGET)
@@ -16,8 +17,7 @@ all: $(TARGET)
 $(TARGET): $(OBJ_FILES)
 	$(CPP) $(CPPFLAGS) -o $@ $^ $(LDFLAGS)
 
-debug: CPPFLAGS += -DDEBUG -g
-debug: LDFLAGS += -g
+debug: CPPFLAGS += -DDEBUG -g -O0
 debug: $(OBJ_FILES)
 	$(CPP) $(CPPFLAGS) -o $(DEBUG_TARGET) $^ $(LDFLAGS)
 
@@ -31,4 +31,3 @@ clean:
 	rm -rf $(BUILD_DIR) $(TARGET) $(DEBUG_TARGET)
 
 .PHONY: all clean debug
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,55 @@
 #include "../include/chip8.hpp"
-#include "../include/display.h"
+#include "../include/display.hpp"
 #include <SDL2/SDL_timer.h>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
-using namespace std;
 
 int main(int argc, char **argv) {
-  // Default Usage message for args
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <rom_name>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-
-  cout << "Starting Chip-8 Emulator...." << "\n";
-
-  // Initialize Emulator Configs
-  Config config;
-  if (!set_config_from_args(&config, argc, argv)) {
-    exit(EXIT_FAILURE);
-  }
-
-  Display display(&config);
-  Audio audio(config);
-
-  const char *rom_name = argv[1];
-  Chip8 chip8(rom_name, &audio);
-
-  display.clear_screen(&config);
-
-  // Seed the random number generator
-  srand(time(nullptr));
-
-  // main emulator loop
-  while (chip8.get_state() != EmulatorState::QUIT) {
-    // handle user input
-    chip8.handle_input(config);
-
-    if (chip8.get_state() == PAUSED)
-      continue;
-
-    // Get time before running instructions
-    const uint64_t start_frame_time = SDL_GetPerformanceCounter();
-
-    // Emulate CHIP8 Instructions for this emulator "frame" (60hz)
-    for (uint32_t i = 0; i < config.insts_per_seconds / 60; i++)
-      chip8.emulate_instruction(config);
-
-    // Get time elapsed after running instructions
-    const uint64_t end_frame_time = SDL_GetPerformanceCounter();
-
-    // delay for approximately 60hz/60fps (16.67ms) or actual time
-    const double time_elapsed =
-        ((double)(end_frame_time - start_frame_time) * 1000) /
-        SDL_GetPerformanceFrequency();
-
-    // SDL_Delay(16 - actual_time_elapsed);
-    SDL_Delay(16.67f > time_elapsed ? 16.67f - time_elapsed : 0);
-
-    // update window with changes every 60hz
-    if (chip8.get_draw_flag()) {
-      display.update_screen(&config, chip8);
-      chip8.set_draw_flag(false);
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <rom_path> [options]\n";
+        return EXIT_FAILURE;
     }
 
-    // update delay & sound timers
-    chip8.update_timers();
+    std::cout << "Starting CHIP-8 Emulator...\n";
 
-    // update delay & sound timers every 60hz
-  }
+    Config config;
+    if (!set_config_from_args(config, argc, argv))
+        return EXIT_FAILURE;
 
-  return 0;
+    Audio audio(config);
+    Display display(config);
+    Chip8 chip8(argv[1], audio);
+
+    display.clear_screen(config);
+
+    while (chip8.get_state() != EmulatorState::QUIT) {
+        chip8.handle_input(config);
+
+        if (chip8.get_state() == EmulatorState::PAUSED)
+            continue;
+
+        const uint64_t frame_start = SDL_GetPerformanceCounter();
+
+        for (uint32_t i = 0; i < config.insts_per_second / 60; ++i)
+            chip8.emulate_instruction(config);
+
+        const uint64_t frame_end = SDL_GetPerformanceCounter();
+        const double elapsed_ms  = static_cast<double>(frame_end - frame_start) * 1000.0 / static_cast<double>(SDL_GetPerformanceFrequency());
+
+        const double target_ms = 1000.0 / 60.0; // ~16.67 ms
+        if (target_ms > elapsed_ms)
+            SDL_Delay(static_cast<uint32_t>(target_ms - elapsed_ms));
+
+        if (chip8.get_draw_flag()) {
+            display.update_screen(config, chip8);
+            chip8.set_draw_flag(false);
+        }
+
+        chip8.update_timers();
+    }
+
+    return EXIT_SUCCESS;
 }
